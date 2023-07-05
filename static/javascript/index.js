3ae96681905b9b8d481dd0c56e3b9144b0d6590c
Sawan <sawankushwah457@gmail.com>
2023-07-05 19:07:28 +0530

    making project clean

---
+++ static/javascript/index.js
@@ -0,0 +1,14 @@
+console.log("hello javascript")
+let element = document.getElementById('element');
+let element2 = document.getElementById('element2');
+
+var typed = new Typed('#element', {
+    strings: ['Front-end developer', 'Bloger', 'Editor', 'C++ developer'],
+    typeSpeed: 50,
+});
+
+
+var typed = new Typed('#element2', {
+    strings: ['Sawan'],
+    typeSpeed: 50,
+});
